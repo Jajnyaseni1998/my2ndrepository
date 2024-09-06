bb8555375eebb3ea1fda27e2a9b1e1d93f4f9f3e
Jajnyaseni1998 <jajnyaseniswain123@gmail.com>
2024-09-06 15:02:27 +0530

    first commit

---
+++ src/main/java/com/ndpro/my2ndProject/App.java
@@ -0,0 +1,13 @@
+package com.ndpro.my2ndProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ndpro/my2ndProject/AppTest.java
@@ -0,0 +1,13 @@
+package com.ndpro.my2ndProject;
+
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest{
+	
+	@Test
+    public void testApp()
+    {
+        System.out.println("hii good morning");
+    }
+}
